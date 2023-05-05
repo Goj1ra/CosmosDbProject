29f31dbe2b642ec7ee395e9fb03aca46a5694aeb
Artem <artem_starchenko_20025@mail.ru>
2023-05-05 21:37:03 +0300

    Add new changes in project

---
+++ CosmosDbProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CosmosDbProject
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            CosmosDbConnection db = new CosmosDbConnection();
+            string command = "";
+            do
+            {
+                Console.WriteLine("Write the CRUD command: ");
+                command = Console.ReadLine();
+                switch (command.ToLower().TrimEnd().TrimStart())
+                {
+
+                    case "create":
+                        await db.CreateUserAsync();
+                        break;
+                    case "delete":
+                        await db.DeleteUserAsync();
+                        break;
+                    case "update":
+                        await db.UpdateUserAsync();
+                        break;
+                    case "query":
+                        await db.GetAppsAsync();
+                        break;
+                }
+            }
+            while (command.ToLower().TrimEnd().TrimStart() != "stop");
+        }
+    }
+}
---
+++ CosmosDbProject/User.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CosmosDbProject
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("surname")]
+        public string Surname { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
